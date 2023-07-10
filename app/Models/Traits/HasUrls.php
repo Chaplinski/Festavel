b46dd8b6514c891ec69ade6360343aa88d241da3
Scott <scott.chaplinski@gmail.com>
2023-07-09 20:49:43 -0500

    adding url trait

---
+++ app/Models/Traits/HasUrls.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Traits;
+
+/**
+ * @property string $website_url
+ * @property string $image_url
+ */
+trait HasUrls
+{
+    //TODO test if @property in this trait works when used by a Model
+}
